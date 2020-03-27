455adf43e12cded0c51dbb8c846b5b759ab180f6
podulator <github-account@podulator.com>
2020-03-27 08:50:19 +0100

    better debug launcher

--- debug.sh
+++ debug.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-#c64debugger -symbols ./start.sym -breakpoints ./main.dbg  -prg ./main.prg
-
-c64debugger -breakpoints ./main.dbg ./main.prg
+c64debugger -symbols ./start.sym -breakpoints ./main.dbg  -prg ./main.prg
